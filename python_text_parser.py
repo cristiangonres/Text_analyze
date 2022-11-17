7178dd5af1b6c33fdc9ca8f55007facfd9e0490c
cristian <gonzalez.resina@gmail.com>
2022-11-17 12:11:04 +0100

    Optimizado y corregido

--- python_text_parser.py
+++ python_text_parser.py
@@ -1,5 +1,4 @@
 import re
-import pprint
 
 def reverse_string(_str):
     print(_str[::-1].capitalize() )
@@ -43,32 +42,27 @@ while 1==1:
     print("5 >> buscar palabra en texto")
     print("6 >> invertir palabras")
     option = input("Seleccione opción: ")
-
+    text = input("Introducir texto: ")
     match option:
         case "1":
-            reverse_string(input("Introducir texto: "))
+            reverse_string(text)
         case "2":
-            _str = input("Introducir texto: ")
             new_vowel = input("¿Que vocal desea usar?")
             while new_vowel not in ['a', 'e', 'i', 'o', 'u']:
                  new_vowel = input("¿Que vocal desea usar?")
-            substitute_vowel(_str, new_vowel)
+            substitute_vowel(text, new_vowel)
         case "3":
-            text = input("Introduzca un texto: \n ").lower()
-            count_char(text)
+            count_char(text.lower())
         case "4":
-            text = input("Introduzca una frase: \n ")
             print("La frase introducida tiene "+ str(count_words(text)) + " palabras")
             print(f'la primera letra es "{text[0]}", la última letra es "{text[-1]}"')
         case "5":
-            word = input("¿Qué palabra desea buscar? \n ").lower()
-            text = input("Introduzca el texto en el que desea encontrar la palabra \n ").lower()
-            if word in text:
+            word = " " + input("¿Qué palabra desea buscar? \n ").lower() + " "
+            if word in " " + text.lower() + " ":
                 print("La palabra SÍ se encuentra en el texto")
             else:
                 print("La palabra NO se encuentra en el texto")
         case "6":
-            text = input("Introduzca una frase: \n ")
             count_words(text, True)
         case _:
             print("Debe seleccionar una de las opciones numéricas del menú.")
